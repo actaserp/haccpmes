f1ba16d8ea7e3804a2ef78159b3956ad687e0ae2
woochul jung <ncdncjf@naver.com>
2025-08-07 18:07:15 +0900

    20250807_정우철_공정별 생산 실적 현황 추가, 창고이동 수정, 생산실적 부적합 현황 수정

--- src/main/java/mes/app/summary/service/ProductionDefectService.java
+++ src/main/java/mes/app/summary/service/ProductionDefectService.java
@@ -22,35 +22,49 @@ public class ProductionDefectService {
 		paramMap.addValue("cboWorkCenter", cboWorkCenter);
 
 		String sql = """
-        with jr_day as (
-            select
-                jr."ProductionDate",
-                jr."WorkCenter_id",
-                sum(coalesce(jr."GoodQty", 0) + coalesce(jr."DefectQty", 0)) as total_prod_qty
-            from job_res jr
-            where jr."ProductionDate" between cast(:date_from as date) and cast(:date_to as date)
-              and jr."State" = 'finished'
-               %s
-            group by jr."ProductionDate", jr."WorkCenter_id"
-        )
-        select
-            dt.id as defect_pk,
-            dt."Name" as defect_type,
-            jr."ProductionDate",
-            sum(jrd."DefectQty")::decimal as defect_qty,
-            jr."WorkCenter_id",
-            jd.total_prod_qty
-        from job_res jr
-            inner join job_res_defect jrd on jrd."JobResponse_id" = jr.id
-            inner join defect_type dt on dt.id = jrd."DefectType_id"
-            inner join jr_day jd
-                on jd."ProductionDate" = jr."ProductionDate"
-               and jd."WorkCenter_id"  = jr."WorkCenter_id"
-        where jr."ProductionDate" between cast(:date_from as date) and cast(:date_to as date)
-          and jr."State" = 'finished'
-          %s
-        group by dt.id, dt."Name", jr."ProductionDate", jr."WorkCenter_id", jd.total_prod_qty
-    """;
+        with jr_day_line as (
+			  select
+				  jr."ProductionDate",
+				  jr."WorkCenter_id",
+				  sum(coalesce(jr."GoodQty", 0) + coalesce(jr."DefectQty", 0)) as line_prod_qty
+			  from job_res jr
+			  where jr."ProductionDate" between cast(:date_from as date) and cast(:date_to as date)
+				and jr."State" = 'finished'
+				%s
+			  group by jr."ProductionDate", jr."WorkCenter_id"
+		  ),
+		  jr_day_total as (
+			  select
+				  jr."ProductionDate",
+				  sum(coalesce(jr."GoodQty", 0) + coalesce(jr."DefectQty", 0)) as total_prod_qty
+			  from job_res jr
+			  where jr."ProductionDate" between cast(:date_from as date) and cast(:date_to as date)
+				and jr."State" = 'finished'
+			  group by jr."ProductionDate"
+		  )
+		  select
+			  dt.id as defect_pk,
+			  dt."Name" as defect_type,
+			  jr."ProductionDate",
+			  sum(jrd."DefectQty")::decimal as defect_qty,
+			  jr."WorkCenter_id",
+			  jd_line.line_prod_qty,
+			  jd_total.total_prod_qty
+		  from job_res jr
+			  inner join job_res_defect jrd on jrd."JobResponse_id" = jr.id
+			  inner join defect_type dt on dt.id = jrd."DefectType_id"
+			  left join jr_day_line jd_line
+				  on jd_line."ProductionDate" = jr."ProductionDate"
+				 and jd_line."WorkCenter_id" = jr."WorkCenter_id"
+			  left join jr_day_total jd_total
+				  on jd_total."ProductionDate" = jr."ProductionDate"
+		  where jr."ProductionDate" between cast(:date_from as date) and cast(:date_to as date)
+			and jr."State" = 'finished'
+			%s
+		  group by dt.id, dt."Name", jr."ProductionDate", jr."WorkCenter_id",
+				   jd_line.line_prod_qty, jd_total.total_prod_qty
+		  
+    	""";
 
 		String wcFilter = (cboWorkCenter != null) ? "and jr.\"WorkCenter_id\" = :cboWorkCenter" : "";
 		sql = String.format(sql, wcFilter, wcFilter);
